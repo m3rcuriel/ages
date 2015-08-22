8a3ac1d0b13549c707213d69ba355d15b0823258
Michael Sandler <jokegred@gmail.com>
2015-08-22 08:48:53 -0700

    SFML image render from a 2d array, no zoom yet

---
+++ ages/OpenGlTest/sfmlTest.cpp
@@ -0,0 +1,44 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+	
+	
+	sf::RenderWindow window(sf::VideoMode(640, 480), "GenericWindow");
+	sf::RectangleShape rectangle(sf::Vector2f(640, 480));
+	//rectangle.setFillColor(sf::Color::Green);
+	
+	
+	sf::Image image;
+	image.create(640, 480);
+	
+	for (int x = 0; x < 640; x++) {
+		for (int y = 0; y < 480; y++) {
+			sf::Color red(255,255,0);
+			image.setPixel(x, y, red);
+		}
+	}
+
+	
+	sf::Texture texture;
+	texture.loadFromImage( image);
+	sf::Sprite sprite(texture);
+	
+	while (window.isOpen()) {
+
+		sf::Event event;
+		
+		while (window.pollEvent(event)) {
+			
+			if (event.type == sf::Event::Closed)
+			window.close();
+		}
+		
+		
+		window.clear(sf::Color::Black);
+		window.draw(sprite);
+		window.display();
+		
+	}
+
+	return 0;
+}
